5e92cb62966493fbff1a54dedf335795111a1a69
ManefMbg <manefbenghorbel@gmail.com>
2024-05-17 20:18:40 +0100

    7th

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,19 +1,63 @@
-import express from 'express';
-import { User } from '../models/User.js';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import express from "express";
+import { User } from "../models/User.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/login', async (req, res) => {
-    const {email, password} = req.body;
-    const user = await User.findOne({email,password})
-    if (!user){
-        res.sendStatus(403)
+
+router.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const foundUser = await User.findOne({ email });
+    if (foundUser) {
+      return res.status(400).send({ msg: "User already exist!!" });
     }
-    res.json(user)
-    console.log("Successful")
+    const newUser = new User({ ...req.body });
+    const saltRounds = 10;
+    const hashPassword = await bcrypt.hash(password, saltRounds);
+    newUser.password = hashPassword;
+    await newUser.save();
 
-}) 
+    const token = jwt.sign(
+      {
+        id: newUser._id,
+      },
+      process.env.SECRET_KEY,
+      { expiresIn: "2h" }
+    );
 
-export {router as AdminRouter}
+    res
+      .status(200)
+      .send({ msg: "User registered successfully...", User: newUser, token });
+  } catch (error) {
+    res.status(500).send({ msg: "User cannot register!!!" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const foundUser = await User.findOne({ email });
+    if (!foundUser)
+      return res.status(400).send({ error: [{ msg: "Bad credential" }] });
+    const checkPassword = await bcrypt.compare(password, foundUser.password);
+    if (!checkPassword)
+      return res.status(400).send({ error: [{ msg: "Bad credential" }] });
+
+    const token = jwt.sign(
+      {
+        id: foundUser._id,
+      },
+      process.env.SECRET_KEY,
+      { expiresIn: "2h" }
+    );
+    res
+      .status(200)
+      .send({ msg: "login successfully .. ", user: foundUser, token });
+  } catch (error) {
+    res.status(400).send({ msg: "User can not login!!!" });
+  }
+});
+
+export { router as AdminRouter };
